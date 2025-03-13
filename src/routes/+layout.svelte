4e83818291c17633efab26717f3e9c274d2d2f35
Shanfan Huang <Shanfan@users.noreply.github.com>
2025-03-13 18:10:00 -0400

    Set up base video player

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,21 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
-	import { webVitals } from '$lib/vitals';
-	import Header from './Header.svelte';
 	import '../app.css';
+	import bgVid from '$lib/videos/bg.mp4';
 
-	let { data, children } = $props();
-
-	$effect(() => {
-		if (data?.analyticsId) {
-			webVitals({
-				path: $page.url.pathname,
-				params: $page.params,
-				analyticsId: data.analyticsId
-			});
-		}
-	});
+	let { children } = $props();
 </script>
 
 <div class="app">
-	<Header />
-
+	<video autoplay muted loop playsinline>
+		<source src={bgVid} type="video/mp4" />
+		Your browser does not support the video tag.
+	</video>
 	<main>
 		{@render children()}
 	</main>
 
 	<footer>
-		<!-- <p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p> -->
+		<p>All rights reserved.</p>
 	</footer>
 </div>
 
@@ -37,6 +24,19 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
+	}
+
+	video {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		min-width: 100vw;
+		min-height: 100vh;
+		width: auto;
+		height: auto;
+		transform: translate(-50%, -50%);
+		object-fit: cover;
+		z-index: -1;
 	}
 
 	main {
@@ -51,15 +51,12 @@
 	}
 
 	footer {
+		color: #eee;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
 	}
 
 	@media (min-width: 480px) {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,9 @@
-<script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
+<script>
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Shanfan Huang personal site" />
 </svelte:head>
 
 <section>
@@ -20,25 +17,10 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
+		color: white;
 	}
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
