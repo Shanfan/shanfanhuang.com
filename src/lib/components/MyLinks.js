cfa2f143b9b9405fb30979d567f9878a6ee7f581
Shanfan Huang <Shanfan@users.noreply.github.com>
2025-03-19 00:30:06 -0400

    fix path to src

--- src/lib/components/MyLinks.js
+++ src/lib/components/MyLinks.js
@@ -3,37 +3,37 @@ export const myLinks = [
         id: 'linkedin',
         url: 'https://www.linkedin.com/in/shanfan-huang/',
         type: 'social',
-        src: '/src/lib/images/linkedin-logo.svg',
+        src: 'src/lib/images/linkedin-logo.svg',
         title: 'LinkedIn'
     }, {
         id: 'instagram',
         url: 'https://www.instagram.com/shanfan.huang',
         type: 'social',
-        src: '/src/lib/images/instagram-logo.svg',
+        src: 'src/lib/images/instagram-logo.svg',
         title: 'Instagram'
     }, {
         id: 'bluesky',
         url: 'https://bsky.app/profile/shanfan.bsky.social',
         type: 'social',
-        src: '/src/lib/images/bluesky-logo.svg',
+        src: 'src/lib/images/bluesky-logo.svg',
         title: 'Blue Sky'
     }, {
         id: 'newsletter',
         url: 'https://picturetextandnumbers.substack.com',
         type: 'creative',
-        src: '/src/lib/images/substack-logo.svg',
+        src: 'src/lib/images/substack-logo.svg',
         title: 'Newsletter'
     }, {
         id: 'behance',
         url: 'https://www.behance.net/shanfanhuang',
         type: 'creative',
-        src: '/src/lib/images/behance-logo.svg',
+        src: 'src/lib/images/behance-logo.svg',
         title: 'Visual Design'
     }, {
         id: 'github',
         url: 'https://shanfan.github.io/',
         type: 'creative',
-        src: '/src/lib/images/github-logo.svg',
+        src: 'src/lib/images/github-logo.svg',
         title: 'Coding'
     }
 ];
