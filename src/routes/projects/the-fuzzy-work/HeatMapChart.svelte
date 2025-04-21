7945148b7bb8662cee339ebf5c033210d8970b95
shanfan <Shanfan@users.noreply.github.com>
2025-04-21 14:16:48 -0400

    finished the heat map (bubble) chat

--- src/routes/projects/the-fuzzy-work/HeatMapChart.svelte
+++ src/routes/projects/the-fuzzy-work/HeatMapChart.svelte
@@ -3,9 +3,16 @@
 	import './style.css';
 	import * as d3 from 'd3';
 	import { getContext } from 'svelte';
+	import { scale } from 'svelte/transition';
 
 	let { data } = $props();
 	let measure = $state('bankrupted');
+
+	let dims = $state({
+		w: null,
+		h: null
+	});
+
 	const colorScale = getContext('stageColor');
 	const stages = getContext('stages');
 	const totalByIndustry = getContext('industryData');
@@ -24,7 +31,25 @@
 		(d) => d.stage
 	);
 
+	const measureMax = {
+		companies: d3.max(pivotMap.values(), (inner) => d3.max(inner.values(), (d) => d.companies)),
+		bankrupted: d3.max(pivotMap.values(), (inner) => d3.max(inner.values(), (d) => d.bankrupted)),
+		ppl_laidoff: d3.max(pivotMap.values(), (inner) => d3.max(inner.values(), (d) => d.ppl_laidoff))
+	};
+
+	function formatK(number) {
+		return number > 1000 ? String(number).slice(0, -3) + 'k' : number;
+	}
+
 	const industries = [...pivotMap.keys()];
+
+	let marginTop = $derived(dims.h / industries.length / 2);
+	let marginLeft = $derived(dims.w / stages.length / 2);
+	let yScale = $derived(d3.scaleBand().range([0, dims.h]).domain(industries));
+	let xScale = $derived(d3.scaleBand().range([0, dims.w]).domain(stages));
+	let rScale = $derived(d3.scaleSqrt().range([0, marginTop]).domain([0, measureMax[measure]]));
+
+	$inspect(measureMax);
 </script>
 
 <div class="main-content">
@@ -47,10 +72,10 @@
 		<p class="header"></p>
 		<div class="stage-grid">
 			{#each stages as stage}
-				<p class="vertical-txt header" style="color: {colorScale(stage)}">{stage}</p>
+				<p class="vertical-text header" style="color: {colorScale(stage)}">{stage}</p>
 			{/each}
 		</div>
-		<p class="vertical-txt header">Total</p>
+		<p class="vertical-text header label-text">Total</p>
 		<!--Body of data -->
 		{#each industries as ind}
 			<p class="right-align">{ind}</p>
@@ -63,13 +88,36 @@
 				{d3.format(',')(totalByIndustry.find((d) => d.key === ind)[measure])}
 			</p>
 		{/each}
-		<div id="svg"></div>
+		<div id="svg" bind:clientWidth={dims.w} bind:clientHeight={dims.h}>
+			<svg width="100%" height="100%">
+				<g class="axis" transform="translate(0, {marginTop})">
+					{#each industries as yAcc}
+						<line x1="0" x2={dims.w} y1={yScale(yAcc)} y2={yScale(yAcc)} />
+					{/each}
+				</g>
+				<g class="circles" transform="translate({marginLeft}, {marginTop})">
+					{#each industries as yAcc}
+						{#each stages as xAcc}
+							{#if pivotMap.get(yAcc).get(xAcc)}
+								<circle
+									r={rScale(pivotMap.get(yAcc).get(xAcc)[measure])}
+									fill={colorScale(xAcc)}
+									cx={xScale(xAcc)}
+									cy={yScale(yAcc)}
+									opacity="0.75"
+								/>
+							{/if}
+						{/each}
+					{/each}
+				</g>
+			</svg>
+		</div>
 		<!-- table footer: summary -->
-		<p class="right-align footer">Total</p>
+		<p class="right-align footer label-text">Total</p>
 		<div class="stage-grid">
 			{#each stages as stage}
 				<p class="center-align footer">
-					{d3.format(',')(totalByStage.find((d) => d.key === stage)[measure])}
+					{formatK(totalByStage.find((d) => d.key === stage)[measure])}
 				</p>
 			{/each}
 		</div>
@@ -87,7 +135,7 @@
 	}
 	#heatmap-chart {
 		display: grid;
-		grid-template-columns: 8em 1fr 5em;
+		grid-template-columns: 8em minmax(0, 1fr) auto;
 		background: #0f0f0f;
 	}
 	#heatmap-chart p {
@@ -95,7 +143,6 @@
 	}
 
 	#svg {
-		background-color: #666;
 		grid-column: 2;
 		grid-row: 2 / span 30;
 	}
@@ -111,13 +158,22 @@
 	.center-align {
 		text-align: center;
 	}
-	.vertical-txt {
+	.vertical-text {
 		writing-mode: vertical-lr;
 		text-orientation: sideways;
-		transform: rotate(180deg);
+		transform: rotate(-150deg) translate(0.5em, 0.25em);
 	}
 	.header,
 	.footer {
-		padding: 0.5em 0;
+		padding: 0.2em 0 0 0;
+	}
+
+	svg {
+		border: 1px solid #555;
+	}
+
+	.axis {
+		stroke-width: 1px;
+		stroke: #555;
 	}
 </style>
