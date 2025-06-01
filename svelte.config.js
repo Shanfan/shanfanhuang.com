972703e05c06ef4c604fc63d930886c2d4f02c58
shanfan <Shanfan@users.noreply.github.com>
2025-05-31 20:04:03 -0400

    add Vercel image optimization

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['*.vercel.app'],
+			}
+		})
 	}
 };
 
