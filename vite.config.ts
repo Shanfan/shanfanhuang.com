74480ebe19be2fd1e003508cb05c96f3d084ae7b
shanfan <Shanfan@users.noreply.github.com>
2025-05-31 20:08:21 -0400

    Adding enhanced-img to optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vitest/config";
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img'
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 export default defineConfig({
-    plugins: [sveltekit(), ViteYaml()],
+    plugins: [
+        enhancedImages(),
+        sveltekit(),
+        ViteYaml(),
+    ],
 
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
